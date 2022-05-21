0b1d300e4ddbd2d8a7df5a3b68131cc31d6d508a
Dmitry <dhr@dbbsoftware.com>
2022-05-21 20:42:50 +0300

    class -> className

--- test/src/Footer.js
+++ test/src/Footer.js
@@ -5,31 +5,31 @@ import { faInstagramSquare, faTwitterSquare, faFacebookSquare } from "@fortaweso
 
 export default function Footer() {
     return (
-      <div class="container">
-        <footer class="py-3 my-4">
-          <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Source code</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Dropbox website</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Dropbox API</a></li>
+      <div className="container">
+        <footer className="py-3 my-4">
+          <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+            <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Source code</a></li>
+            <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Dropbox website</a></li>
+            <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Dropbox API</a></li>
           </ul>
-          <div class="d-flex justify-content-between py-4 my-4">
+          <div className="d-flex justify-content-between py-4 my-4">
             <p>&copy; 2022 DIY, Inc. All rights reserved.</p>
-            <ul class="list-unstyled d-flex">
-              <li class="ms-3">
+            <ul className="list-unstyled d-flex">
+              <li className="ms-3">
                 <i className="fab fa-instagram">
                   <FontAwesomeIcon icon={faInstagramSquare} />
                   <span style={{ marginLeft: "10px" }}>
                     Instagram
                   </span>
                 </i></li>
-              <li class="ms-3">
+              <li className="ms-3">
                 <i className="fab fa-twitter">
                     <FontAwesomeIcon icon={faFacebookSquare} />
                     <span style={{ marginLeft: "10px" }}>
                       Facebook
                     </span>
                   </i></li>
-              <li class="ms-3"><i className="fab fa-twitter">
+              <li className="ms-3"><i className="fab fa-twitter">
                   <FontAwesomeIcon icon={faTwitterSquare} />
                   <span style={{ marginLeft: "10px" }}>
                     Twitter
--- test/src/Header.js
+++ test/src/Header.js
@@ -6,10 +6,10 @@ import './Header.css'
 
 export default function Header() {
     return (
-    <div class="container p-1 my-1">
-        <nav class="navbar navbar-blue">
-            <a class="navbar-brand" href="#">
-                <img src={logo} alt="" width="250" height="50" class="d-inline-block align-text-top"/>
+    <div className="container p-1 my-1">
+        <nav className="navbar navbar-blue">
+            <a className="navbar-brand" href="#">
+                <img src={logo} alt="" width="250" height="50" className="d-inline-block align-text-top"/>
                 Demo
             </a>
         </nav>
