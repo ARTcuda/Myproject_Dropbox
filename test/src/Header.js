c1864b5e87da8aa541a788c8109c8dc20f176766
Art <42577308+ARTcuda@users.noreply.github.com>
2022-05-29 16:22:25 +0300

    commit on 'main'

--- test/src/Header.js
+++ test/src/Header.js
@@ -5,6 +5,11 @@ import './Header.css'
 
 
 export default function Header() {
+
+    const handleClickLogin = () => {
+        window.location.href="https://dropbox.com/oauth2/authorize?response_type=token&client_id=ka73scg36t1bp9y&redirect_uri=http://localhost:3000/"
+    }
+
     return (
     <div className="container p-1 my-1">
         <nav className="navbar navbar-blue">
@@ -12,7 +17,7 @@ export default function Header() {
                 <img src={logo} alt="" width="250" height="50" className="d-inline-block align-text-top"/>
                 Demo
             </a>
-            <button class="btn btn-secondary btn-lg" type="submit">Login</button>
+            <button onClick={handleClickLogin} className="btn btn-info" role="button">Login</button>
         </nav>
     </div>
     )
