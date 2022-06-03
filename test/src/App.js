8e79726d45fd5483e5232645ee15037f1841e810
Art <42577308+ARTcuda@users.noreply.github.com>
2022-06-03 15:54:00 +0300

    commit on "main"

--- test/src/App.js
+++ test/src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [list, setList] = useState([])
   const [cursor, setCursor] = useState('')
   const [selected, setSelected] = useState([])
+  const [isLoggedIn, setisLoggedIn] = useState(false)
 
   const showAlert = (a) => {
     alert(a);
@@ -33,36 +34,49 @@ function App() {
   }
 
 
-
+  //wtf
   const handleClickLogin = async() => {
     window.location.href="https://dropbox.com/oauth2/authorize?response_type=token&client_id=ka73scg36t1bp9y&redirect_uri=http://localhost:3000/"
-    //console.log(window.location.pathname)
+    //console.log(window.location.pathname) єто не работает т.к. я открьіваю ссьілку там еще 3 перехода и нужно уже после єтих переходов получать урл
     //const token=smtUrlParser(window.location.href)
     //throwTokenToBack(token)
-    //wtf
+
   }
 
   return (
     <div className="App">
       <Header />
-      <button onClick={handleClickLogin} className="btn btn-info">Login</button>
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center'
-      }}>
-        {list.map((item) => <FileItem
-          key={item.id}
-          item={item}
-          selected={selected.includes(item.id)}
-          setSelected={setSelected}
-        />)}
-      </div>
-      <button onClick={loadMore} type="button" className="btn btn-primary">Load more</button>
+      {isLoggedIn ? (
+         <><div className="container py-3">
+            <div className="btn-group d-block float-end" role="group">
+              <button type="button" className="btn btn-secondary">Upload</button>
+              <button type="button" className="btn btn-secondary">Move</button>
+              <button type="button" className="btn btn-secondary">Delete</button>
+            </div>
+          </div><div className="container py-3">
+              {list.map((item) => <FileItem
+                key={item.id}
+                item={item}
+                selected={selected.includes(item.id)}
+                setSelected={setSelected} />)}
+            </div><button onClick={loadMore} type="button" className="btn btn-primary">Load more</button></>
+      ) : (
+        <div className="container py-3" >
+          <div className="card">
+            <div className="card-header bg-danger d-flex justify-content-center ">
+              You are not logged in!
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">If you want to use this application you should be logged in</h5>
+              <p className="card-text">To log in, please click on button bellow. You will be redirected to official Dropbox website with log in form.</p>
+              <button className="btn btn-success mx-auto d-block" onClick={handleClickLogin} >Login</button>
+            </div>
+          </div>
+        </div>
+      )}
       <Footer/>
     </div>
-    
-  ); 
+  )
 }
 
-export default App;
+export default App
