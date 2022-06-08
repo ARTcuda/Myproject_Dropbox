7db2b62d4a8f6dedf57b3fa8f42f88392f332f84
Art <42577308+ARTcuda@users.noreply.github.com>
2022-06-08 17:39:34 +0300

    Update back end side in these file: package-lock.json, package.json, and 6 more files...

--- test-BE/middlewares/dropbox.js
+++ test-BE/middlewares/dropbox.js
@@ -1,59 +1,54 @@
-const res = require('express/lib/response')
 const { Readable } = require('stream')
 require('isomorphic-fetch')
-const Dropbox = require('dropbox').Dropbox
+const { Dropbox } = require('dropbox')
 
 const listFiles = async (req, res, next) => {
   try {
-    const { cursor, limit = process.env.COUNT } = req.query
-    const dbx = new Dropbox({ accessToken: process.env.DROPBOX_TOKEN })
-    const { result } = await dbx.filesListFolder({path: '', limit })
+    const { limit = process.env.COUNT, dropboxToken } = req.query
+    const dbx = new Dropbox({ accessToken: dropboxToken })
+    const { result } = await dbx.filesListFolder({ path: '', limit })
     res.send({ entries: result.entries, cursor: result.cursor })
-  } catch(err) {
+  } catch (err) {
     next(err)
   }
 }
 
-const getMoreFiles = async  (req, res, next)  => {
-  try{
-    const { cursor } = req.query
-    const dbx = new Dropbox({ accessToken: process.env.DROPBOX_TOKEN })
-    const { result }  = await dbx.filesListFolderContinue({ cursor })
+const getMoreFiles = async (req, res, next) => {
+  try {
+    const { cursor, dropboxToken } = req.query
+    const dbx = new Dropbox({ accessToken: dropboxToken })
+    const { result } = await dbx.filesListFolderContinue({ cursor })
     res.send({ entries: result.entries, cursor: result.cursor })
-  } catch(err) {
+  } catch (err) {
     next(err)
   }
 }
 
 const downloadFile = async (req, res, next) => {
-  try{
-    const { path } = req.query
+  try {
+    const { path, dropboxToken } = req.query
 
     if (!path || !path.length) {
       throw new Error('path is not defined')
     }
 
-    const dbx = new Dropbox({ accessToken: process.env.DROPBOX_TOKEN})
+    const dbx = new Dropbox({ accessToken: dropboxToken })
     const file = await dbx.filesDownload({ path })
 
     const stream = Readable.from(file.result.fileBinary)
     return stream.pipe(res)
-  } catch(err) {
-    if (err.status == 409) {
+  } catch (err) {
+    if (err.status === 409) {
       const error = new Error()
       error.status = 404
       return next(error)
     }
     return next(err)
   }
-} 
-
-/*const receiveTokenFromFront = (token) =>{
-
-}*/
+}
 
 module.exports = {
   listFiles,
   downloadFile,
-  getMoreFiles
+  getMoreFiles,
 }
